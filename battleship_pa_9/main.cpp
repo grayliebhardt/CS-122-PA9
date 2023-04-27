d432d8ad1c6b8c30bc4963bc16f8484aeebe476c
Grayson Liebhardt <108197334+grayliebhardt@users.noreply.github.com>
2023-04-26 19:17:38 -0700

    Update main.cpp

--- battleship_pa_9/main.cpp
+++ battleship_pa_9/main.cpp
@@ -1,8 +1,13 @@
 #include "header.hpp"
-
+/*
+Class: CS 122
+Assignment: PA 9
+Group: Grayson Liebhardt, 
+Program: Battleship Game
+*/
 int main(void)
 {
-	gameWrapper();
+	gameWrapper();	//Runs game
 	
 	return 0;
 }
